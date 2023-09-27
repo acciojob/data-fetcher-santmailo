cc440fea3b8b93cbc9ffa383d5be3728604b6c0f
santmailo <iamsunil006@gmail.com>
2023-09-28 00:22:44 +0530

    fetched the data and displayed as soon as fetched

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = React.useState("");
+
+  useEffect(()=> {
+
+    axios.get("https://dummyjson.com/products")
+    .then((data)=> setData(data.data))
+    .catch((err) => console.log(err));
+  },[])
+
   return (
     <div>
         {/* Do not remove the main div */}
+        {JSON.stringify(data)}
     </div>
   )
 }
