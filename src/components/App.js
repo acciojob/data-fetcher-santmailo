47a4ee8fc29393a517967ead867f1dc2424ab4e0
santmailo <iamsunil006@gmail.com>
2023-09-28 00:45:47 +0530

    done changing elements to made them more visible

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,30 @@ import './../styles/App.css';
 
 const App = () => {
   const [data, setData] = React.useState("");
+  const [err, setErr] = React.useState("");
+
 
   useEffect(()=> {
 
-    axios.get("https://dummyjson.com/products")
+    axios.get("https://dummyjson.com/pdroducts")
     .then((data)=> setData(data.data))
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      // console.log(err.message);
+      setErr(err.message)
+    });
   },[])
 
   return (
     <div>
         {/* Do not remove the main div */}
-        <pre>
-          {JSON.stringify(data)}
-        </pre>
+
+        {
+          (err!="") ? <p>{err}</p> :
+          <pre>
+            <h1>Data Fetched from API</h1>
+            {JSON.stringify(data)}
+          </pre>
+        }
     </div>
   )
 }
