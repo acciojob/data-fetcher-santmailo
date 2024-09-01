781f667b7a7ce6430327021031d95e9c7a516f28
santmailo <iamsunil006@gmail.com>
2024-09-01 20:44:59 +0530

    attempt 2 august

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,35 @@
-
 import React, { useEffect } from "react";
 import axios from "axios";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
   const [data, setData] = React.useState("");
   const [err, setErr] = React.useState("");
 
-
-  useEffect(()=> {
-
-    axios.get("https://dummyjson.com/products")
-    .then((data)=> setData(data.data))
-    .catch((err) => {
-      // console.log(err.message);
-      setErr(err.message)
-    });
-  },[])
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((data) => setData(data.data))
+      .catch((err) => {
+        // console.log(err.message);
+        setErr(err.message);
+      });
+  }, []);
 
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
 
-        {
-          (err!="") ? <p>{err}</p> :
-          <pre>
-            <h1>Data Fetched from API</h1>
-            {JSON.stringify(data)}
-          </pre>
-        }
+      {data.length == 0 ? (
+        <p>Loading...</p>
+      ) : (
+        <pre>
+          <h1>Data Fetched from API</h1>
+          {JSON.stringify(data)}
+        </pre>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
